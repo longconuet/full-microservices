048272cc4e671b059fbc0b50c3bae036debb6918
longnt <nice231096@gmail.com>
2024-06-12 00:42:41 +0700

    add serilog to all API prj

--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,25 +1,56 @@
+using Common.Logging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Log.Logger = new LoggerConfiguration()
+//    .WriteTo.Console()
+//    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog(Serilogger.Configure);
 
-var app = builder.Build();
+Log.Information("Starting Product API up...");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+try
+{  
+    // Serilog
+    //builder.Host.UseSerilog((context, configuration) => configuration
+    //    .WriteTo.Console(
+    //        outputTemplate: 
+    //        "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+    //    .Enrich.FromLogContext()
+    //    .ReadFrom.Configuration(context.Configuration));
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shutdown Product API complete...");
+    Log.CloseAndFlush();
+}
